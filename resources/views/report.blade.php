ea8ec87791682c1cf6e2a02fbbc0af58d404c68b
yutanoda <atuyuta902@yahoo.co.jp>
2020-09-16 17:28:09 +0900

    もとに戻した

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -72,7 +72,7 @@
 								<dt>{{ $report_detail->detail_number }}.</dt>
 							</dl>
 							<dl>
-								<dt>営業</dt>
+								<dt>営業日</dt>
 								<dd>
 									<input type="date" name="action_date" value="{{ $report_detail->action_date }}" @if ( $t_report->status_flag >= 1 ) onfocus="this.blur()" readonly="readonly" @endif>
 								</dd>
